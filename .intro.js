59e6227b89dfb072531597c839931461aadbb69f
Meiguro <meiguro@meiguro.com>
2014-07-26 01:34:30 -0700

    Add window.addEventListener load event shim for PebbleKit JS

--- .intro.js
+++ .intro.js
@@ -6,3 +6,11 @@ var Meteor;
 var Deps;
 var DDP;
 
+if (document.addEventListener && !window.addEventListener) {
+  window.addEventListener = function(eventType, callback) {
+    if (eventType === 'load') {
+      setTimeout(callback, 0);
+    }
+  };
+}
+
